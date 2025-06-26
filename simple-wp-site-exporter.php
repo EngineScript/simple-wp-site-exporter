b63c66915b1ce6242d5d31c50a01b03fa4aaa549
Peter Downey <peterdowney@visistruct.com>
2025-06-26 17:36:34 -0400

    Release

--- simple-wp-site-exporter.php
+++ simple-wp-site-exporter.php
@@ -2,7 +2,7 @@
 /*
     Plugin Name: Simple WP Site Exporter
     Description: Exports the site files and database as a zip archive.
-    Version: 1.7.0
+    Version: 1.8.0
     Author: EngineScript
     License: GPL v3 or later
     Text Domain: Simple-WP-Site-Exporter
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Define plugin version.
 if ( ! defined( 'ES_WP_SITE_EXPORTER_VERSION' ) ) {
-    define( 'ES_WP_SITE_EXPORTER_VERSION', '1.7.0' );
+    define( 'ES_WP_SITE_EXPORTER_VERSION', '1.8.0' );
 }
 
 /**
